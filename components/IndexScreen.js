463f258ee10d46bdc0fc725b5a36625c864f6023
Eren AVAR <eren.avar@hotmail.com>
2023-11-29 11:24:27 +0300

    Inserted React Navigation and Created Screens

---
+++ components/IndexScreen.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+export default function IndexScreen() {
+  return (
+    <View>
+      <Text>Index Screen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
