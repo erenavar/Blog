84bad518688ed9777222b27867142451c367a1b4
Eren AVAR <eren.avar@hotmail.com>
2023-11-30 13:24:32 +0300

    Added useReducer

--- context/BlogContext.js
+++ context/BlogContext.js
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { useReducer } from "react";
+
 const BlogContext = React.createContext();
 
+blogReducer = (state, action) => {
+  switch (action.type) {
+    case "add_blogpost":
+      return [...state, { title: "Vue.Js" }];
+
+    default:
+      return state;
+  }
+};
+
 export const BlogProvider = ({ children }) => {
-  const blogPosts = [{ title: "React Native" }, { title: "Javascript" }];
+  const [blogPosts, dispatch] = useReducer(blogReducer, [
+    { title: "C#" },
+    { title: " Model-View-Controller" },
+  ]);
+
+  const addBlogPost = () => {
+    dispatch({ type: "add_blogpost" });
+  };
+
   return (
-    <BlogContext.Provider value={blogPosts}>{children}</BlogContext.Provider>
+    <BlogContext.Provider value={{ data: blogPosts, addBlogPost }}>
+      {children}
+    </BlogContext.Provider>
   );
 };
 
--- screens/IndexScreen.js
+++ screens/IndexScreen.js
@@ -3,13 +3,14 @@ import React, { useContext } from "react";
 import BlogContext from "../context/BlogContext";
 
 export default function IndexScreen() {
-  const blogPosts = useContext(BlogContext);
+  const { data, addBlogPost } = useContext(BlogContext);
 
   return (
     <View>
       <Text>IndexScreen</Text>
+      <Button title="Add" onPress={addBlogPost} />
       <FlatList
-        data={blogPosts}
+        data={data}
         keyExtractor={(blogPosts) => blogPosts.title}
         renderItem={({ item }) => {
           return <Text>{item.title}</Text>;
